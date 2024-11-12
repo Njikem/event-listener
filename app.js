ae4b688f67a91b6c5cfcfcd9c7ce9a6389f1592d
Njikem <njikemmaureen88@gmail.com>
2024-11-12 14:56:08 -0700

    event

--- app.js
+++ app.js
@@ -27,23 +27,90 @@ function hTag1(){
     
     // Insert the new h3 element after the h1 element
     hTag.insertAdjacentElement('afterend', newH3);
+
+    //Remove theh3 tag if it exist
+
+    newH3.addEventListener('click', h3Tag);
+
+    function h3Tag(){
+        if(newH3){
+            newH3.remove();
+        }
+    }
+
+   
 }
 
 
 //Add mouseleave listener to remove h3
 
-   let hTag2 = document.querySelector('h-tag');
+  //  hTag.addEventListener('mouseleave', removeH3);
 
-    hTag2.addEventListener('mouseleave', removeH3);
-
-function removeH3(){
+//unction removeH3(){
 
   //Remove the h3 tag if it exist
 
-    if(hTag2){
-        hTag2.remove();
-    }
+ //   if(h3){
+     //   h3.remove();
+   // }
 
+//
+
+
+//Key down event
+document.body.addEventListener('keydown', bodyTag1);
+
+function bodyTag1(event){
+      if(event.key.toLowerCase() === 'p'){
+        document.body.style.backgroundColor = 'Purple';
+      }
 }
+
+//Key Up event
+document.body.addEventListener('keyup', keyUp);
+
+function keyUp(event){
+    if(event.key.toLowerCase() === 'p'){
+
+        document.body.style.backgroundColor = 'white'
+
+    }
+    
+}
+
+// Select all buttons
+let buttons = document.querySelectorAll('button');
+
+
+//Add event listner for each button
+
+buttons.forEach(button => {
+
+    button.addEventListener('click', function () {
+
+          //Create an h1 element
+    let h1Tag = document.createElement('h1');
+
+    h1Tag.textContent = `You Selected: ${button.value}`;
+
+
+
+    //Inject an h1 element into the body
+    document.body.appendChild(h1Tag);
+
+    //Remove the h1 tag when the item is selected
+    h1Tag.addEventListener('click', function() {
+        if(h1Tag){
+            h1Tag.remove();
+        }
+    })
+
+    })
+})
+
+
+
+   
+
 
 
